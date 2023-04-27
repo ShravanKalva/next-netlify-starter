2b604872f0bae210449cd257eec10479d9db1018
ShravanKalva <10042932+ShravanKalva@users.noreply.github.com>
2023-04-27 13:31:38 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my Sample Next.js app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing Samples <code>pages/index.js</code>
         </p>
       </main>
 
